99d16a4fb1ff50546471833bb59ecabdae07e641
Dan Reddell <dannoreddell@gmail.com>
2016-04-18 17:13:44 -0700

    no message

--- config.php
+++ config.php
@@ -4,16 +4,16 @@ $mastertitle="Stuff";
 $tagline="write your crap down";
 
 
-$dbconfig['host']='localhost';
-$dbconfig['dbuser']='root';
-$dbconfig['dbpass']='password';
-$dbconfig['database']='know';
+$dbconfig['host']='';
+$dbconfig['dbuser']='';
+$dbconfig['dbpass']='';
+$dbconfig['database']='';
 
 
-$ldapconfig['bindhost']='10.128.14.34';
+$ldapconfig['bindhost']='';
 $ldapconfig['bindport']=636;
 $ldapconfig['sasl']=true;
-$ldapconfig['baseou']='ou=people,dc=urbanairship,dc=com';
+$ldapconfig['baseou']='';
 $ldapconfig['attributes']['username']=array('name'=>'username','ldapname'=>'username');
 $ldapconfig['attributes']['email']=array('name'=>'email','ldapname'=>'email');
 $ldapconfig['attributes']['fname']=array('name'=>'fname','ldapname'=>'given_name');
